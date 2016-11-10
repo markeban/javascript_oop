9ee99c7dff54908de16957176bdcc83a764d3f9c
Mark Richardson <mark@markeban.com>
2016-11-09 16:06:14 -0800

    var counter 'dilemma'

---
+++ oop.js
@@ -0,0 +1,13 @@
+var counter = 0;
+
+function clickBoxOne() {
+  counter++;
+  console.log("Box One clicked " + counter + " times.")
+}
+
+var counter = 0;
+
+function clickBoxTwo() {
+  counter++;
+  console.log("Box One clicked " + counter + " times.")
+}
